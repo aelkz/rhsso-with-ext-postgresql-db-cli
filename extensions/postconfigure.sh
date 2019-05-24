94e7868935a2c931309e12984cf975eb59fc7c58
Luigi De Masi <luigi.demasi@extrasys.it>
2019-05-24 17:54:05 +0200

    Update postconfigure.sh

--- extensions/postconfigure.sh
+++ extensions/postconfigure.sh
@@ -2,5 +2,3 @@
 echo "Executing actions.cli"
 $JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/actions.cli
 echo "Done"
-echo "new standalone-openshift.xml:"
-cat /opt/eap/standalone/configuration/standalone-openshift.xml
