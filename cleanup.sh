6656d99b7c73fcddbf866592d46a8194fd700707
Luigi De Masi <luigi.demasi@extrasys.it>
2019-05-24 17:02:15 +0200

    first commit

---
+++ cleanup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#EXIT ON ERROR
+#set -e
+
+rm  ca.cer  ca.key  ca.srl  httpskey.jceks  jgroups.jceks sso.cer  sso-request.cer truststore.jks
+
+
+echo "switch to ${SSO_NAMESPACE} project"
+error=$(oc project ${SSO_NAMESPACE})
+
+if [ $? -eq 0 ]; then
+    echo "OK! "
+else
+    echo $error
+    exit 1
+fi
+
+#oc delete secret env-datasource
+oc delete secret db-cli-script
+oc delete secret sso-app-secret
+oc policy remove-role-from-user view system:serviceaccount:$(oc project -q):sso-service-account
+oc delete serviceaccount sso-service-account
+
+oc delete all --all -n sso
---
+++ extensions/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -x
+echo "Running $PWD/install.sh"
+injected_dir=$1
+cp -rf ${injected_dir} $JBOSS_HOME/extensions
+cp /tmp/extensions/actions.cli $JBOSS_HOME/extensions
+
